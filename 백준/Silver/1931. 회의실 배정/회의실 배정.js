47db2106d34b5bf787701adb4ddc11ce482381d8
YangGwangSeong <52439610+YangGwangSeong@users.noreply.github.com>
2024-03-23 18:53:08 +0900

    [Silver I] Title: 회의실 배정, Time: 440 ms, Memory: 47480 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,36 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, ...arr] = input;
+const times = arr
+  .map((num) => num.split(" ").map((num) => +num))
+  .sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    } else {
+      return a[1] - b[1];
+    }
+  });
+
+function solution(numberArr) {
+  let answer = 0;
+
+  numberArr.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - [1];
+  });
+
+  let endTime = 0;
+  for (let item of numberArr) {
+    if (item[0] >= endTime) {
+      answer++;
+      endTime = item[1];
+    }
+  }
+
+  return answer;
+}
+console.log(solution(times));

bcb2fbb93d215d0b8f6f5b29e141798210db1a68
yanggwangseong <52439610+yanggwangseong@users.noreply.github.com>
2024-09-10 18:11:24 -0700

    [Silver II] Title: 사탕 게임, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,35 +1,31 @@
-import sys
-def get_best():
+def get_best(y, x):
     global N, matrix
     best = 0
 
     # columns
-    for i in range(N):
-        bef = '-'
-        value = 0
-        for j in range(N):
-            if bef == matrix[i][j]:
-                value += 1
-            else:
-                value = 1
-            bef = matrix[i][j]
-            best = max(best, value)
+    bef = '-'
+    value = 0
+    for j in range(N):
+        if bef == matrix[y][j]:
+            value += 1
+        else:
+            value = 1
+        bef = matrix[y][j]
+        best = max(best, value)
 
     # rows
-    for j in range(N):
-        bef = '-'
-        value = 0
-        for i in range(N):
-            if bef == matrix[i][j]:
-                value += 1
-            else:
-                value = 1
-            bef = matrix[i][j]
-            best = max(best, value)
+    bef = '-'
+    value = 0
+    for i in range(N):
+        if bef == matrix[i][x]:
+            value += 1
+        else:
+            value = 1
+        bef = matrix[i][x]
+        best = max(best, value)
 
     return best
 
-input = sys.stdin.readline
 
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
@@ -40,13 +36,15 @@ ans = 0
 
 for y in range(N):
     for x in range(N):
+        if y == x:
+            ans = max(ans, get_best(y, x))
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
             if 0 <= ny < N and 0 <= nx < N:
                 if matrix[y][x] != matrix[ny][nx]:
                     matrix[y][x], matrix[ny][nx] = matrix[ny][nx], matrix[y][x]
-                    ans = max(ans, get_best())
+                    ans = max(ans, get_best(y, x))
                     matrix[y][x], matrix[ny][nx] = matrix[ny][nx], matrix[y][x]
 
 print(ans)

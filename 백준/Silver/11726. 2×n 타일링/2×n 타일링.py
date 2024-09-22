2a6f3e942adebc403289980ae105b10969ce6ea6
yanggwangseong <52439610+yanggwangseong@users.noreply.github.com>
2024-09-21 17:54:12 -0700

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,12 +1,14 @@
-# input
+### bottom-up 방식
 N = int(input())
 
-# solve
-dp = [0] * (N + 2)
+# dp 테이블 설계 
+# dp[n] = dp[n-1] + dp[n-2]
+
+dp = [-1] * (N + 2)
 dp[1] = 1
 dp[2] = 2
 
-for n in range(3, N + 1):
-	dp[n] = (dp[n-1] + dp[n-2]) % 10007
+for i in range(3, N+1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007
 
 print(dp[N])

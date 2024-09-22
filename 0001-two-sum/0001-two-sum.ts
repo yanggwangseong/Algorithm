87a96f984fa51684299e81d4b3a1b9f7a3bfba0d
yanggwangseong <52439610+yanggwangseong@users.noreply.github.com>
2024-09-21 23:35:44 -0700

    Delete 0001-two-sum directory

--- 0001-two-sum/0001-two-sum.ts
+++
@@ -1,6 +0,0 @@
-function twoSum(nums: number[], target: number): number[] {
-    for (let i = 0; i < nums.length; i++) {
-        const j = nums.lastIndexOf(target - nums[i]);
-        if (j !== -1 && i !== j) return [i, j];
-    }
-};

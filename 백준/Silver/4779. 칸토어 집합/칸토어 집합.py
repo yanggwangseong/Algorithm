1d402faf83ba636a6fddc0a2586c81e5941cfb2e
yanggwangseong <52439610+yanggwangseong@users.noreply.github.com>
2024-10-29 05:17:26 -0700

    [Silver III] Title: 칸토어 집합, Time: 32 ms, Memory: 32156 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,18 +1,11 @@
-def func(k):
-	# base case
-	if k == 0:
-		print('-', end="")
-		return
+ans = ['' for _ in range(13)]
+ans[0] = '-'
 
-	# recursive case
-	func(k-1)
-	print(' ' * (3 ** (k-1)), end="")
-	func(k-1)
+for i in range(1, 13):
+    ans[i] = ans[i-1] + (' ' * (3 ** (i-1))) + ans[i-1]
 
 while True:
-	try:
-		N = int(input())
-		func(N)
-		print()
-	except:
-		break
+    try:
+        print(ans[int(input())])
+    except:
+        break
